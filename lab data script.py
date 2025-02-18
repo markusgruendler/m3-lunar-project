471edc83a2e22f11a4155c14e78dc93baa5630fb
markus-gruendler <mgruendleroutreach@gmail.com>
2025-02-18 02:23:14 -0800

    1) Part A to script

--- lab data script.py
+++ lab data script.py
@@ -21,16 +21,27 @@ else:
 
 print(f"# of file paths: {len(file_paths_list)}" + f"\n" + f"first: {file_paths_list[0]}" + f" \n" + f"last:  {file_paths_list[-1]}")
 
+
 # assumed step size between m3 points
-STEPSIZE = 0.04
+STEPSIZE = 0.01
 
 # boundaries for wavelengths, outside of which to ignore/truncate data
-WL_MIN = 0.46
-WL_MAX = 2.94
+WL_MIN = 0.446
+WL_MAX = 2.99
 
-m3 = pd.read_csv('m3 original data.csv')
+m3_path = 'clark_m3_lab_spectra/Clark et al M3_Wavelengths_center channel or mean of Gaussian.txt'
+
+# 20250202: using clark files as wavelength bin centers
+m3 = pd.read_csv(m3_path, skiprows=3)
+
+m3.columns = ['Wavelength (µm)']
+
 x_m3 = (m3['Wavelength (µm)']).tolist()
 x_m3 = [f"{wl:.5f}" for wl in x_m3]
+print(f"# of m^3 wavelengths: {len(x_m3)}, first and last 10: \n{x_m3[0:10]}, \n{x_m3[-1-10:-1]}")
+
+WL_MIN = float(x_m3[0])-STEPSIZE/2
+WL_MAX = float(x_m3[-1])+STEPSIZE/2
 # print(f"# of m^3 wavelengths: {len(x_m3)}, first and last: {x_m3[0], x_m3[-1]}")
 
 for file_path in file_paths_list:
@@ -45,6 +56,8 @@ for file_path in file_paths_list:
     y_column_label = lab.columns[1]
 
     x_raw = list(lab.index)
+    # x_raw rounding to 5 digits and recast to float (optional)
+    x_raw = [float(f"{wl:.5f}") for wl in x_raw]
     y_raw = list(lab[y_column_label])
 
     # print(f"first and last raw x,y pairs\n{(x_raw[0], y_raw[0])}\n{(x_raw[-1], y_raw[-1])}")
@@ -89,8 +102,8 @@ for file_path in file_paths_list:
     x_avg = []
     for count, bin in enumerate(binned_raw):
         if len(binned_raw[bin]) == 0:
-            x_avg.append(bin)
-            y_avg.append(-1)
+            # x_avg.append(bin)
+            # y_avg.append(-1)
             print(f"Empty bin at {bin}")
             continue
 
@@ -104,7 +117,10 @@ for file_path in file_paths_list:
         avgy /= len(binned_raw[bin])
 
         y_avg.append(avgy)
-        x_avg.append(avgx)
+        # takes average x wl
+        # x_avg.append(avgx)
+        # takes band center
+        x_avg.append(float(bin))
 
     # print(f"# of average reflectance values {len(y_avg)}, first and last averaged points {[(x_avg[0], y_avg[0]), (x_avg[-1], y_avg[-1])]}")
 
@@ -114,14 +130,22 @@ for file_path in file_paths_list:
     spline_points = len(x_avg)
     cs_avg = CubicSpline(x_avg, y_avg)
 
-    x_spline_avg = list(np.linspace(min(x_avg), max(x_avg), spline_points))
+    x_spline_avg = list(np.linspace(x_avg[0], x_avg[-1], spline_points))
     y_spline_avg = list(cs_avg(x_spline_avg))
 
     ### writing file
+    count = 0
+    while len(x_raw) != len(x_avg) or len(x_raw) != len(x_spline_avg):
+        x_avg.append('')
+        y_avg.append('')
+        x_spline_avg.append('')
+        y_spline_avg.append('')
+        count += 1
+    
     def writeFile(path, mode): # x = new, w = overwrite
         output = open(path, mode)
 
-        output.write(f"Wavelength raw,{y_column_label} raw,Wavelength bin avg,{y_column_label} bin avg,Wavelength cubic spline of bin avg,{y_column_label} cubic spline of bin avg\n")
+        output.write(f"Wavelength (µm) raw,{y_column_label} raw,Wavelength (µm) bin avg,{y_column_label} bin avg,Wavelength (µm) cubic spline of bin avg,{y_column_label} cubic spline of bin avg\n")
 
         # ends when shortest zip input runs out
         for xr, yr, xavg, yavg, xspline, yspline in zip(x_raw, y_raw, x_avg, y_avg, x_spline_avg, y_spline_avg):
@@ -135,7 +159,7 @@ for file_path in file_paths_list:
         print(output_path)
     except:
         writeFile(output_path,'w')
-        # print(f'file present for {output_path}')
+        print(f'file overwritten for {output_path}')
         pass
 
 print("process finished")
